91d91ece099d334bdc988cb555e0ffd68fba837e
Zsolt Fejes <zsolt.fejes@amito.com>
2022-04-04 13:46:48 +0100

    Finished UI changes

--- update_style.js
+++ update_style.js
@@ -3,10 +3,11 @@ pic_container = document.getElementById('pic_container')
 pic_container.style.display = "flex";
 pic_container.style.height = '90vh';
 
-// Change width to retain aspect ratio
-img = document.getElementsByTagName('img')[0];
-img.style.width = 'auto';
-img.style.minWidth = null;
-img.style.maxWidth = null;
-console.log(img.style)
-console.log("Style update completed")
+var imgs = document.getElementById('pic_container').querySelectorAll('img')
+
+imgs.forEach(element => {
+  console.log(element.src)
+  element.style.width = 'auto';
+  element.style.minWidth = 'unset';
+});
+
