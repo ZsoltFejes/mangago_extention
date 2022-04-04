6470d9070644f0d5c7b765b8f064e67c3c0aba71
Zsolt Fejes <zsolt.fejes@icloud.com>
2022-04-04 19:06:56 +0100

    Finished key listener and auto sroll

--- update_style.js
+++ update_style.js
@@ -1,13 +1,26 @@
-// Update overall size
-pic_container = document.getElementById('pic_container')
-pic_container.style.display = "flex";
-pic_container.style.height = '90vh';
+function update_style() {
+  // Update overall size
+  pic_container = document.getElementById('pic_container')
+  pic_container.style.display = "flex";
+  pic_container.style.height = '95vh';
 
-var imgs = document.getElementById('pic_container').querySelectorAll('img')
+  var imgs = document.getElementById('pic_container').querySelectorAll('img')
 
-imgs.forEach(element => {
-  console.log(element.src)
-  element.style.width = 'auto';
-  element.style.minWidth = 'unset';
+  imgs.forEach(element => {
+    if (!element.classList.contains('loading')){
+      console.log(element)
+      element.style.width = 'auto';
+      element.style.minWidth = 'unset';
+    }
+  });
+  window.scroll({
+    top: 130,
+    behavior: 'smooth'
+  })
+}
+
+setTimeout(update_style, 1000)
+
+document.addEventListener('keyup', (e) => {
+  setTimeout(update_style, 1000)
 });
-
