d94fe30de5697900b8ddcf8b2cac88d755995c33
Zsolt Fejes <zsolt.fejes@amito.com>
2022-04-04 12:20:22 +0100

    Picture container height is set correctly

--- update_style.js
+++ update_style.js
@@ -1,12 +1,12 @@
 // Update overall size
-img_container = document.getElementById('pic_container')
-img_container.style.display = "flex";
-img_container.style.height = '90vh';
+pic_container = document.getElementById('pic_container')
+pic_container.style.display = "flex";
+pic_container.style.height = '90vh';
 
 // Change width to retain aspect ratio
-imgs = document.getElementsByTagName('img');
-for (let i = 0; i < imgs.length; i++){
-  imgs[i].style.width = auto;
-  imgs[i].style.minWidth = null;
-  imgs[i].style.maxWidth = null;
-}
+img = document.getElementsByTagName('img')[0];
+img.style.width = 'auto';
+img.style.minWidth = null;
+img.style.maxWidth = null;
+console.log(img.style)
+console.log("Style update completed")
